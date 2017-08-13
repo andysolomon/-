4dc5ced600a4e28b72994614d90e2f121c05f450
asolomon <andrew.solomon@coxautoinc.com>
2017-08-13 12:33:36 -0400

    Add in Apollo 🌝 for graphql queries.

--- native/app/index.js
+++ native/app/index.js
@@ -1,13 +1,25 @@
-
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// 1
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
 
+// 2 Create a networkInterface with createNetworkInterface
+// const networkInterface = createNetworkInterface({
+//   uri: 'https://api.graph.cool/simple/v1/cj6avpqx1037i01704u86qvvn'
+// })
+
+// 3 Create a client which accepts your networkInterface
+const client = new ApolloClient();
+
+// 4 Wrap App component in a ApolloProvider HOC passing the client as a prop.
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Let's get lunch🍔!</Text>
-      </View>
+      <ApolloProvider client={client}>
+        <View style={styles.container}>
+          <Text>Let's get lunch🍔!</Text>
+        </View>
+      </ApolloProvider>
     );
   }
 }
